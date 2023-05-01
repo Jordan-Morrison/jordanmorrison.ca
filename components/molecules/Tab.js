a6682364ec18a2ef138fe3b269170b60c858ea92
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2023-05-01 17:25:48 -0400

    Fixed tab accessibility issue
    Links were wrapped around the outside of the list items

--- components/molecules/Tab.js
+++ components/molecules/Tab.js
@@ -3,8 +3,8 @@ import styles from '@/styles/Tab.module.css';
 
 export default function Tab(props) {
     return (
-        <Link className={styles.link} href={`/${props.tab.slug}`}>
-            <li className={`${styles.tab} ${props.active ? styles.active : null}`} onClick={() => props.onTabClick(props.tab.slug)}>{props.tab.name}</li>
-        </Link>
+        <li className={`${styles.tab} ${props.active ? styles.active : null}`} onClick={() => props.onTabClick(props.tab.slug)}>
+            <Link href={`/${props.tab.slug}`}>{props.tab.name}</Link>
+        </li>
     )
 }
