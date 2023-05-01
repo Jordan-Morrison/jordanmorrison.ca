6692b52f6082fc18cf1caf94bc9aed6892e45233
Jordan Morrison <32398038+Jordan-Morrison@users.noreply.github.com>
2023-05-01 19:24:26 -0400

    Updating base url for images

--- components/helpers/helpers.js
+++ components/helpers/helpers.js
@@ -5,7 +5,7 @@ export async function getImages(category) {
     let images = await readdirSync(`./public/images/${category}`, {withFileTypes: true})
     .filter(item => !item.isDirectory())
     .filter(item => ["jpeg", "jpg", "png"].some(extension => item.name.includes(extension)))
-    .map(item => `/images/${category}/` + item.name);
+    .map(item => `${process.env.IMAGE_BASE_URL}/images/${category}/` + item.name);
 
     return images;
 }
